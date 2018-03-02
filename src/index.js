5c6d0501d0e3655ca2cd21cb04527bfe112c5224
recep_hidir <recep.hidir@gmail.com>
2018-03-02 13:34:42 +0300

    HMR Support added.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import {sayHello} from './print';
+
 console.log("Hi all..!");
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("content").innerHTML = "whateverX";
+    document.getElementById("content").innerHTML = "whateverK";
 });
+
+/*if (module.hot) {
+  module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        sayHello();
+      })
+}*/
